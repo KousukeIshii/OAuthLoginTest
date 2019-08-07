c7e94775fb432c8e0bf4945ef3658a85de2e8f4d
石井康介 <faitayahhuu@yahoo.co.jp>
2019-08-07 18:07:34 +0900

    Route設定

--- oauth-test/routes/web.php
+++ oauth-test/routes/web.php
@@ -15,6 +15,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/auth/{service}', 'OAuthLoginController@getGoogleAuth')->where('service', 'google');
+Route::get('/auth/callback/google', 'OAuthLoginController@authGoogleCallback');
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
