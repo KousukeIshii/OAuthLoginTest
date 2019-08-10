f4015aba5dfe48b7db7d6405db3dc2a834e74079
石井康介 <faitayahhuu@yahoo.co.jp>
2019-08-11 01:43:03 +0900

    Googleログインボタンの変更

--- oauth-test/resources/views/auth/login.blade.php
+++ oauth-test/resources/views/auth/login.blade.php
@@ -1,9 +1,13 @@
-@extends('layouts.app')
-
+@extends('layouts/app')
 @section('content')
-<div class="container">
-    <a class=”btn btn-block btn-social btn-google” href="auth/google">
-        <i class=”fab fa-google” aria-hidden=”true”> Googleアカウントでログイン</i>
-    </a>
+<div class="container col-md-4">
+    <div class="card">
+        <div class="card-header">ログイン</div>
+        <div class="card-body">
+            <a class="btn btn-block btn-social btn-google-inverse" href="auth/google">
+                <i class="fab fa-google" aria-hidden="true"> Googleアカウントでログイン</i>
+            </a>
+        </div>
+    </div>
 </div>
 @endsection
